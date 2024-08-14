bbe4d9a0f3b5e637ceb5b7b7c8b188861bfc5eb0
Gian Aguilar <gian.aguilar89@gmail.com>
2024-08-14 18:02:07 -0500

    primera version restserver con typescript, solo con gestión de usuarios

--- db/connection.ts
+++ db/connection.ts
@@ -3,7 +3,7 @@ import {Sequelize} from "sequelize";
 const db = new Sequelize("tsrestserver","root","35P3r4n24", {
     host: "localhost",
     dialect: "mysql",
-    //logging: false
+    logging: false
 })
 
 export default db;
